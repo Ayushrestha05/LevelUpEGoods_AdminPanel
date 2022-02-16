d546dd28549e459cf66d093463e9117b1688ef2b
Ayush Shrestha <ayushrestha05@gmail.com>
2022-02-16 22:43:19 +0545

    Created Item Model and Migration

---
+++ app/Models/Item.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    use HasFactory;
+    protected $table = 'items';
+
+    public function Music(){
+        return $this->hasOneThrough(Music::class,Item::class,'id','item_id');
+    }
+}
