626591d4780475cbddffbd37065e41dd656b1c04
Ayush Shrestha <ayushrestha05@gmail.com>
2022-03-02 21:56:50 +0545

    Added Colors to Category Seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -16,27 +16,35 @@ class CategorySeeder extends Seeder
     {
         Categories::create([
             'category_name' => 'Gift Cards',
+            'category_color' => '#5e5e5e',
         ]);
         Categories::create([
             'category_name' => 'Illustrations',
+            'category_color' => '#aa3eff',
         ]);
         Categories::create([
             'category_name' => 'Figurines',
+            'category_color' => '#ffc226',
         ]);
         Categories::create([
             'category_name' => 'Games',
+            'category_color' => '#736464',
         ]);
         Categories::create([
             'category_name' => 'Playstation Exclusives',
+            'category_color' => '#008cff',
         ]);
         Categories::create([
             'category_name' => 'Switch Exclusives',
+            'category_color' => '#ff3535',
         ]);
         Categories::create([
             'category_name' => 'Music',
+            'category_color' => '#b4a2a2',
         ]);
         Categories::create([
             'category_name' => 'All Products',
+            'category_color' => '#2a6a92',
         ]);
 
     }
